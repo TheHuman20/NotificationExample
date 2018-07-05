df8acd5fa9e2ec05f273d6a8f250192e6b882b8a
TheHuman20 <osad.sanya@gmail.com>
2018-07-05 23:46:49 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/osads/notificationexample/MainActivity.java
+++ app/src/main/java/com/example/osads/notificationexample/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManager nm;
-    private final int NOTIFICATION_ID = 127;    //Magic number
+    private final int NOTIFICATION_ID = 100;  //Magic number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
     }
-
-//    public void onClickNotification(View view) {
-
-//        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-//
-//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext())
-//                .setContentIntent(pendingIntent)
-//                .setSmallIcon(R.drawable.ic_launcher_foreground)
-//                .setLargeIcon(BitmapFactory.decodeResource(getApplication().getResources(), R.drawable.ic_launcher_foreground))
-//                .setTicker("Some string")
-//                .setWhen(System.currentTimeMillis())
-//                .setAutoCancel(true)
-//                .setContentTitle("New notification")
-//                .setContentText("Some details of notification")
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .setProgress(100, 20, true);
-//        long[] vibrate = new long[]{100, 1000, 100, 1000};
-//        Notification notification = notificationBuilder.build();
-//        notification.defaults = notification.DEFAULT_SOUND;
-//        notification.vibrate = vibrate;
-//        notification.flags = notification.flags | Notification.FLAG_INSISTENT | Notification.FLAG_ONGOING_EVENT;
-//        nm.notify(NOTIFICATION_ID, notification);
-
 
     public void onClickNotification(View view) {
 
@@ -60,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
                 .setContentTitle("Message")
-                .setContentText("Very important message")
+                .setContentText("Important message")
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setProgress(100, 50, false);
         long[] vibrate = new long[]{1000, 1500, 1000, 1500};
